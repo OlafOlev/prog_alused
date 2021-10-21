d4e11a3ef1f581b1de37810ed64118ba93c9e73d
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2021-10-21 11:23:29 +0300

    1.5 Nädala ajakulu

---
+++ praks1/1.5 Nädala ajakulu.py
@@ -0,0 +1,5 @@
+ainepunktide_arv = int(input("Sisestage ainepunktide arv: "))
+nädalate_arv = int(input("Sisestage nädalate arv: "))
+tunnid = ainepunktide_arv * 26
+nädala_ajakulu = tunnid / nädalate_arv
+print("Teie ajakulu nädalas on " + str(nädala_ajakulu))
