5fcbcb8a3681ee3b91c0eecf772392dd6c0677e3
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2021-10-22 06:18:08 +0300

    tegin koodi paremaks

--- praks1/2.3 Istekoht.py
+++ praks1/2.3 Istekoht.py
@@ -3,12 +3,12 @@ if iseloos == "ise":
     valikoht = input("Kas soovite istuda akna ääres(aken) või mitte (muu)?")
     if valikoht == "aken":
         print("Valisite ise. Aknakoht")
-    if valikoht == "muu":
+    elif valikoht == "muu":
         print("Valisite ise. Vahekäigukoht")
 
-if iseloos == "loos":
-    import random
-    r1 = random.randint(1, 3)
+elif iseloos == "loos":
+    from random import randint
+    r1 = randint(1, 3)
     if (r1 == 1) or (r1 == 2):
         print("Istekoht loositi. Vahekäigukoht")
     else:
