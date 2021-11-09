e7f8ad8b81bffd96a18f27a464061228d3aeb358
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2021-11-09 14:25:08 +0200

    4.3 Liiklusmärk

---
+++ praks1/4.3 Liiklusmärk.py
@@ -0,0 +1,9 @@
+from tkinter import *
+raam = Tk()
+raam.title("Peatee märk")
+tahvel = Canvas(raam, width = 1000, height = 1000)
+#välis must ruut
+tahvel.create_polygon(500, 0, 1000, 500, 500, 1000, 0, 500, 500, 0, fill = "white",width = 10, outline="black", )
+tahvel.create_polygon(500, 100, 900, 500, 500, 900, 100, 500, 500, 100, fill = "yellow",width = 8, outline="black", )
+tahvel.pack()
+raam.mainloop()
