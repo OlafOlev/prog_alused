2c7a3482822ac054a03b9f72a0c4cd8bd165a85b
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2021-11-30 13:11:29 +0200

    6.6 Kuupäev

---
+++ praks1/6.6 Kuupäev.py
@@ -0,0 +1,9 @@
+def kuu_nimi(mitmes):
+    kuud = ["jaanuar", "veebruar", "märts", "april", "mai", "juuni", "juuli", "august", "septemper", "oktoober", "november", "detsember"]
+    return kuud[int(mitmes) - 1]
+def kuupäev_sõnena(kuupäev):
+    kuupäevad = kuupäev.split(".")
+    kuupäev = kuupäevad[0] + ". " + kuu_nimi(kuupäevad[1]) + " " + kuupäevad[2] + ". a"
+    return kuupäev
+kuupäev = input("Sisesta kuupäev kujul DD.MM.YYYY: ")
+print(str(kuupäev_sõnena(kuupäev)))
